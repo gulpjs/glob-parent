9ef8a87f66b1a43d0591e7a8e4fc5a18415ee388
Elan Shanker <elan.shanker@gmail.com>
2016-10-18 17:47:30 -0400

    feat: pre-process braces/brackets sections
    Replace with simple * so that whatever path part they are in still gets
    recognized as containing glob magic

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var pathDirname = require('path-dirname');
 
 module.exports = function globParent(str) {
 	str += 'a'; // preserves full path in case of trailing path separator
+	// replace braces/brackets sections with *
+	str = str.replace(/(^|[^\\])(\{([^{}]*?)}|\[([^\[\]]*?)\])/g, '$1*');
 	do {str = pathDirname.posix(str)} while (isglob(str));
 	return str.replace(/\\([\*\?\|\[\]\(\)\{\}])/g, '$1');
 };
