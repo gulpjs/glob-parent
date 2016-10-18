40225c19a36308d4452466226a38e4d629cafdfb
Elan Shanker <elan.shanker@gmail.com>
2016-10-18 17:48:02 -0400

    Clean up formatting
    comment on each operation

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ var isglob = require('is-glob');
 var pathDirname = require('path-dirname');
 
 module.exports = function globParent(str) {
-	str += 'a'; // preserves full path in case of trailing path separator
+	// preserves full path in case of trailing path separator
+	str += 'a';
+
 	// replace braces/brackets sections with *
 	str = str.replace(/(^|[^\\])(\{([^{}]*?)}|\[([^\[\]]*?)\])/g, '$1*');
+
+	// remove path parts that are globby
 	do {str = pathDirname.posix(str)} while (isglob(str));
+
+	// remove escape chars and return result
 	return str.replace(/\\([\*\?\|\[\]\(\)\{\}])/g, '$1');
 };
