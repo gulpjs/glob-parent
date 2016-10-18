8cfb0ba84202d51571340dcbaf61b79d16a26c76
Elan Shanker <elan.shanker@gmail.com>
2016-10-18 18:16:40 -0400

    feat: preserve escaped brace/bracket at end of string

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var isglob = require('is-glob');
 var pathDirname = require('path-dirname');
 
 module.exports = function globParent(str) {
-	// preserves full path in case of trailing path separator
-	str += 'a';
-
 	// replace braces/brackets sections with *
 	str = str.replace(/(^|[^\\])(\{([^{}]*?)}|\[([^\[\]]*?)\])/g, '$1*');
+	if (/[\}\}]$/.test(str)) str += '/';
+
+	// preserves full path in case of trailing path separator
+	str += 'a';
 
 	// remove path parts that are globby
 	do {str = pathDirname.posix(str)} while (isglob(str));
