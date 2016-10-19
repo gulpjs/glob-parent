1a7b3e8f36adfcf96849e096fb1252ad1e6c09d6
Elan Shanker <elan.shanker@gmail.com>
2016-10-19 10:45:12 -0400

    New approach to braces/brackets handling

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var isglob = require('is-glob');
 var pathDirname = require('path-dirname');
 
 module.exports = function globParent(str) {
-	// replace braces/brackets sections with *
-	str = str.replace(/(^|[^\\])(\{([^{}]*?)}|\[([^\[\]]*?)\])/g, '$1*');
-	if (/[\}\]]$/.test(str)) str += '/';
+	// special case for strings ending in enclosure containing path separator
+	if (/[\{\[].*[\/]*.*[\}\]]$/.test(str)) str += '/';
 
 	// preserves full path in case of trailing path separator
 	str += 'a';
 
 	// remove path parts that are globby
-	do {str = pathDirname.posix(str)} while (isglob(str));
+	do {str = pathDirname.posix(str)}
+	while (isglob(str) || /(^|[^\\])[\{\[]/.test(str));
 
 	// remove escape chars and return result
 	return str.replace(/\\([\*\?\|\[\]\(\)\{\}])/g, '$1');
