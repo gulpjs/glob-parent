137add8481f2fb629e702111174d5a191357592c
Paul Miller <paul@paulmillr.com>
2019-03-27 14:05:20 +0200

    Update: Hoist regexps and strings for performance gains

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 
-var isglob = require('is-glob');
+var isGlob = require('is-glob');
 var pathDirname = require('path-dirname');
 var isWin32 = require('os').platform() === 'win32';
 
+var slash = '/';
+var backslash = /\\/g;
+var enclosure = /[\{\[].*[\/]*.*[\}\]]$/;
+var globby = /(^|[^\\])([\{\[]|\([^\)]+$)/;
+var escaped = /\\([\*\?\|\[\]\(\)\{\}])/g;
+
 module.exports = function globParent(str) {
   // flip windows path separators
-  if (isWin32 && str.indexOf('/') < 0) {
-    str = str.split('\\').join('/');
+  if (isWin32 && str.indexOf(slash) < 0) {
+    str = str.replace(backslash, slash);
   }
 
   // special case for strings ending in enclosure containing path separator
-  if (/[\{\[].*[\/]*.*[\}\]]$/.test(str)) {
-    str += '/';
+  if (enclosure.test(str)) {
+    str += slash;
   }
 
   // preserves full path in case of trailing path separator
@@ -21,8 +27,8 @@ module.exports = function globParent(str) {
   // remove path parts that are globby
   do {
     str = pathDirname.posix(str);
-  } while (isglob(str) || /(^|[^\\])([\{\[]|\([^\)]+$)/.test(str));
+  } while (isGlob(str) || globby.test(str));
 
   // remove escape chars and return result
-  return str.replace(/\\([\*\?\|\[\]\(\)\{\}])/g, '$1');
+  return str.replace(escaped, '$1');
 };
