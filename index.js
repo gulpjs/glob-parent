896c0c00b4e7362f60b96e7fc295ae929245255a
Blaine Bublitz <blaine.bublitz@gmail.com>
2019-03-29 15:27:49 -0700

    chore!: Drop support for node <6 & bump dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var isGlob = require('is-glob');
-var pathDirname = require('path-dirname');
+var pathPosixDirname = require('path').posix.dirname;
 var isWin32 = require('os').platform() === 'win32';
 
 var slash = '/';
@@ -26,7 +26,7 @@ module.exports = function globParent(str) {
 
   // remove path parts that are globby
   do {
-    str = pathDirname.posix(str);
+    str = pathPosixDirname(str);
   } while (isGlob(str) || globby.test(str));
 
   // remove escape chars and return result
