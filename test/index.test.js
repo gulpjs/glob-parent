f923d5c4fb629dda881c2d6eb9e7a547d2de2e66
sttk <sttk.xslet@gmail.com>
2023-07-01 16:38:08 +0900

    fix: support windows path with drive or unc volume

--- test/index.test.js
+++ test/index.test.js
@@ -10,6 +10,10 @@ describe('glob-parent', function () {
     expect(gp('.*')).toEqual('.');
     expect(gp('/.*')).toEqual('/');
     expect(gp('/.*/')).toEqual('/');
+    expect(gp('//')).toEqual('/');
+    expect(gp('//*')).toEqual('/');
+    expect(gp('.//')).toEqual('./');
+    expect(gp('.//*')).toEqual('./');
     expect(gp('a/.*/b')).toEqual('a');
     expect(gp('a*/.*/b')).toEqual('.');
     expect(gp('*/a/b/c')).toEqual('.');
@@ -254,6 +258,73 @@ if (isWin32) {
   describe('technically invalid windows globs', function () {
     it('should manage simple globs with backslash path separator', function (done) {
       expect(gp('C:\\path\\*.js')).toEqual('C:/path');
+
+      done();
+    });
+  });
+
+  describe('windows path with drive or UNC volume', function() {
+    it('should return parent dirname from absolute path with drive letter', function(done) {
+      expect(gp('C:/')).toEqual('C:/');
+      expect(gp('C:/.')).toEqual('C:/');
+      expect(gp('C:/*')).toEqual('C:/');
+      expect(gp('C:/./*')).toEqual('C:/.');
+      expect(gp('C://')).toEqual('C:/');
+      expect(gp('C://*')).toEqual('C:/');
+      expect(gp('C:/path/*.js')).toEqual('C:/path');
+
+      expect(gp('C:\\')).toEqual('C:/');
+      expect(gp('C:\\.')).toEqual('C:/');
+      expect(gp('C:\\*')).toEqual('C:/');
+      expect(gp('C:\\.\\*')).toEqual('C:/.');
+      expect(gp('C:\\\\')).toEqual('C:/');
+      expect(gp('C:\\\\*')).toEqual('C:/');
+      expect(gp('C:\\path\\*.js')).toEqual('C:/path');
+
+      done();
+    });
+
+    it('should return parent dirname from relative path with drive letter', function(done) {
+      expect(gp('C:')).toEqual('C:.');
+      expect(gp('C:.')).toEqual('C:.');
+      expect(gp('C:*')).toEqual('C:.');
+      expect(gp('C:./*')).toEqual('C:.');
+      expect(gp('C:.//')).toEqual('C:./');
+      expect(gp('C:.//*')).toEqual('C:./');
+      expect(gp('C:path/*.js')).toEqual('C:path');
+
+      expect(gp('C:.\\*')).toEqual('C:.');
+      expect(gp('C:.\\\\')).toEqual('C:./');
+      expect(gp('C:.\\\\*')).toEqual('C:./');
+      expect(gp('C:path\\*.js')).toEqual('C:path');
+
+      done();
+    });
+
+    it('should return parent dirname from UNC path', function(done) {
+      expect(gp('\\\\System07\\C$/')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/.')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/*')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/./*')).toEqual('\\\\System07\\C$/.');
+      expect(gp('\\\\System07\\C$//')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$//*')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/path/*.js')).toEqual('\\\\System07\\C$/path');
+
+      expect(gp('\\\\System07\\C$/', { flipBackslashes: false })).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/.', { flipBackslashes: false })).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/*', { flipBackslashes: false })).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/./*', { flipBackslashes: false })).toEqual('\\\\System07\\C$/.');
+      expect(gp('\\\\System07\\C$//', { flipBackslashes: false })).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$//*', { flipBackslashes: false })).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$/path/*.js')).toEqual('\\\\System07\\C$/path');
+
+      expect(gp('\\\\System07\\C$\\')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$\\.')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$\\*')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$\\.\\*')).toEqual('\\\\System07\\C$/.');
+      expect(gp('\\\\System07\\C$\\\\')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$\\\\*')).toEqual('\\\\System07\\C$/');
+      expect(gp('\\\\System07\\C$\\path\\*.js')).toEqual('\\\\System07\\C$/path');
 
       done();
     });
